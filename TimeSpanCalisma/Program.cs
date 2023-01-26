304eb58dcc66058021e762d5ac811b2af31beeea
yusufboyaci <boyacyusuf@gamil.com>
2023-01-26 22:04:04 +0300

    Add project files.

---
+++ TimeSpanCalisma/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeSpanCalisma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan span = TimeSpan.Parse("8:30");
+            Console.WriteLine(span);
+            Console.ReadKey();
+        }
+    }
+}
